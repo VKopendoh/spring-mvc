7bdf216d3ee8e4bd9e8e8df2b537ce76191e3024
vkopendoh <vkopendoh@bk.ru>
2019-09-08 22:43:34 +0300

    Handle with a white spaces in the input form - best Spring practice

--- spring-mvc/src/com/vkopendoh/springmvc/mvc/CustomerController.java
+++ spring-mvc/src/com/vkopendoh/springmvc/mvc/CustomerController.java
@@ -2,15 +2,31 @@ package com.vkopendoh.springmvc.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	//add an @InitBinder (this metho will pre-process every String form data
+	//remove leading and trailing whitespace and if String only has whitespace rim it to null)
+	//to convert trim input strings 
+	//resolve issue for validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+	}
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
